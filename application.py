415198c2efe11f7ec1a9cabbd8129c3cdde52ad9
Stanley <cslafond@gmail.com>
2020-05-01 15:04:30 -0400

    validate forms

--- application.py
+++ application.py
@@ -13,9 +13,12 @@ def home():
 def about():
     return render_template ('about.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for { form.username.data }!','success')
+        return redirect(url_for('home'))
     return render_template ('register.html', form=form)
 
 @app.route('/login')
--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2,max=20)])
     email = StringField('Email', validators=[DataRequired()]) 
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Username', validators=[DataRequired(),EqualTo('pasword')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
