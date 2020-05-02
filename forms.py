de1d393ceac4bd5460410449bb72ef74b2ccb2a2
Stanley <cslafond@gmail.com>
2020-05-02 11:52:04 -0400

    Final form validation

--- forms.py
+++ forms.py
@@ -3,14 +3,15 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2,max=20)])
-    email = StringField('Email', validators=[DataRequired()]) 
+    fname = StringField('First Name', validators=[DataRequired(), Length(min=2,max=20)])
+    lname = StringField('Last Name', validators=[DataRequired(), Length(min=2,max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()]) 
+    email = StringField('Email', validators=[DataRequired(), Email()]) 
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
